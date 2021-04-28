d7d79ee9aae8bb06ed72fd4f82708fd8b7b86f84
sunghocipherome <34561419+sunghocipherome@users.noreply.github.com>
2021-04-28 16:21:19 +0900

    초기환경 완료

---
+++ src/main/java/com/imc/website/ImcWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.imc.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImcWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImcWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imc/website/ImcWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imc.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
